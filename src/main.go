abcffb6e695b8ca99f7df234d17f83427d0454a8
Petarda-Atomica <perfect.minecraft.andrei@gmail.com>
2024-09-28 18:01:58 +0300

    Added a landing page and implemented a very primitive model selection page

--- src/main.go
+++ src/main.go
@@ -6,6 +6,8 @@ import (
 	"image/color"
 	"log"
 	"os"
+	"path"
+	"strings"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -13,22 +15,59 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"golang.org/x/image/colornames"
 )
 
-var logger bytes.Buffer
+// Images
+var ICON_robot_hand_round *canvas.Image
 
+// Variables
+var logger bytes.Buffer
+var workingDirectory string
+
+// Fyne variables
 var App fyne.App
 var MainWindow fyne.Window
 
-func initialize() {
+func bigBTN(text string, extraPadding float32, size float32, f func()) *fyne.Container {
+	btnText := canvas.NewText(text, theme.Color(theme.ColorNameForeground))
+	btnText.TextSize = size
+	btnBG := widget.NewButton(" ", f)
+	btnBGHeightIncrements := 1
+	for {
+		desiredValue := fyne.MeasureText(btnText.Text, btnText.TextSize, btnText.TextStyle)
+		actualValue := fyne.MeasureText(btnBG.Text, theme.TextSize(), btnText.TextStyle)
+		// fmt.Println("Desired: ", desiredValue, "\nActual: ", actualValue)
+		if desiredValue.Width+extraPadding > actualValue.Width {
+			btnBG.Text += " "
+			btnBG.Refresh()
+			continue
+		}
+
+		if desiredValue.Height > actualValue.Height*float32(btnBGHeightIncrements) {
+			btnBG.Text += "\n"
+			btnBGHeightIncrements++
+			btnBG.Refresh()
+			// continue
+		}
+
+		break
+	}
+	btn := container.New(layout.NewCenterLayout(), btnBG, btnText)
+	return btn
+}
+
+// * Particular windows
+func startup(minLoadingTime int) {
 	// Initialize logger
 	log.SetOutput(&logger)
 	log.Println("Initialized a new logger")
 
 	// Render a splash screen while the app is loading
-	log.Println("Starting rendering splash screen...")
+	log.Println("Rendering splash screen...")
 	var splashScreen fyne.Window
 	var status *canvas.Text
 	if drv, ok := fyne.CurrentApp().Driver().(desktop.Driver); ok {
@@ -39,24 +78,27 @@ func initialize() {
 		// Draw the background
 		background := canvas.NewImageFromFile("splash.png")
 		background.Resize(fyne.NewSize(1024, 512)) // Resize image to desired size
-		background.Move(fyne.NewPos(0, 0))         // Set the position for the image
 
 		// Draw text
 		status = canvas.NewText("Loading application...", colornames.Orangered)
 		status.TextSize = 20
-		status.Move(fyne.NewPos(10, 512-35))
 
 		// Draw rectangle
 		rectangle := canvas.NewRectangle(color.RGBA{R: 9, G: 9, B: 9, A: 255})
 		rectangle.Resize(fyne.NewSize(1024, 40))
-		rectangle.Move(fyne.NewPos(0, 512-40))
 
 		// Assemble UI
 		content := container.NewWithoutLayout(background, rectangle, status)
+		background.Move(fyne.NewPos(0, 0))
+		status.Move(fyne.NewPos(10, 512-35))
+		rectangle.Move(fyne.NewPos(0, 512-40))
 
 		// Show splash screen
 		splashScreen.SetContent(content)
 		splashScreen.Resize(fyne.NewSize(1024, 512))
+		rectangle.Refresh()
+		status.Refresh()
+		splashScreen.CenterOnScreen()
 		splashScreen.Show()
 		MainWindow.Hide()
 	} else {
@@ -66,19 +108,33 @@ func initialize() {
 	log.Println("Started loading")
 	startTime := time.Now()
 
+	// Update status in real time
+	go func() {
+		prevLog := ""
+		for {
+			if prevLog == logger.String() {
+				continue
+			}
+			prevLog = logger.String()
+
+			entries := strings.Split(logger.String(), "\n")
+			status.Text = entries[len(entries)-2]
+			status.Refresh()
+			fmt.Println(entries[len(entries)-2])
+		}
+	}()
+
 	// Some init
-	// TODO: Init some stuff
+	Init()
 
 	// Hide splash screen
 	deltaTime := time.Since(startTime)
 	status.Text = fmt.Sprintf("Finished loading in %s!", deltaTime.String())
 	log.Println(status.Text)
 	status.Refresh()
-	time.Sleep(time.Second*1 - deltaTime)
+	time.Sleep(time.Second*time.Duration(minLoadingTime) - deltaTime)
 	splashScreen.Hide()
-	MainWindow.Show()
 }
-
 func fatalError(err error) {
 	App.SendNotification(fyne.NewNotification("Crash report", "Machina-Maestro ran into a problem and crashed"))
 	const popupSize = 512
@@ -134,10 +190,160 @@ func fatalError(err error) {
 	errorPopup.Show()
 }
 
+// Init function
+func Init() {
+	var err error
+
+	// Get working directory
+	log.Println("Fetching working directory...")
+	workingDirectory, err = os.Getwd()
+	if err != nil {
+		fatalError(err)
+	}
+
+	// Reading Icons
+	ICON_robot_hand_round = canvas.NewImageFromFile(path.Join(workingDirectory, "/assets/robot_hand_round.png"))
+}
+
+// Main page
+func landingPage(window fyne.Window) {
+	// Make title
+	title := canvas.NewText("Machina Maestro", colornames.Orange)
+	title.Alignment = fyne.TextAlignCenter
+	title.TextSize = 32
+
+	// Make icon
+	ICON_robot_hand_round.SetMinSize(fyne.NewSquareSize(512))
+	ICON_robot_hand_round.FillMode = canvas.ImageFillContain
+
+	// Make button
+	btn := bigBTN("Select your OVI", 40, 20, func() {
+		modelSelectWindow(window)
+	})
+
+	// Assemble UI
+	content := container.New(
+		layout.NewVBoxLayout(),
+		layout.NewSpacer(),
+		container.New(layout.NewHBoxLayout(), layout.NewSpacer(), title, layout.NewSpacer()),
+		layout.NewSpacer(),
+		container.New(layout.NewHBoxLayout(), layout.NewSpacer(), ICON_robot_hand_round, layout.NewSpacer()),
+		layout.NewSpacer(),
+		container.New(layout.NewHBoxLayout(), layout.NewSpacer(), btn, layout.NewSpacer()),
+		layout.NewSpacer(),
+	)
+	window.SetContent(content)
+	window.Show()
+	window.CenterOnScreen()
+}
+
+func modelSelectWindow(window fyne.Window) {
+	// Make a navbar
+	btnBack := widget.NewButton("", func() { landingPage(window) })
+	btnBack.SetIcon(theme.Icon(theme.IconNameNavigateBack))
+	btnAdd := widget.NewButton("", func() {})
+	btnAdd.OnTapped = func() {
+		modelAddWindow(window, btnAdd)
+		btnAdd.Disable()
+	}
+	btnAdd.SetIcon(theme.Icon(theme.IconNameContentAdd))
+	navbar := container.New(
+		layout.NewStackLayout(),
+		canvas.NewRectangle(theme.Color(theme.ColorNameButton)),
+		container.New(
+			layout.NewHBoxLayout(),
+			btnBack,
+			layout.NewSpacer(),
+			btnAdd,
+		),
+	)
+
+	// Make a title
+	title := canvas.NewText("Select an OVI", theme.Color(theme.ColorNameForeground))
+	title.TextSize = 32
+	title.Alignment = fyne.TextAlignCenter
+
+	// Arrange and draw
+	content := container.New(
+		layout.NewVBoxLayout(),
+		navbar,
+		title,
+	)
+	window.SetContent(content)
+}
+
+func modelAddWindow(window fyne.Window, btnAdd *widget.Button) {
+	// Make a new window
+	addWindow := App.NewWindow("Add a new model")
+	addWindow.SetOnClosed(func() { btnAdd.Enable() })
+
+	// Make a navbar
+	btnBack := widget.NewButton("", func() { addWindow.Close() })
+	btnBack.SetIcon(theme.Icon(theme.IconNameNavigateBack))
+	navbar := container.New(
+		layout.NewStackLayout(),
+		canvas.NewRectangle(theme.Color(theme.ColorNameButton)),
+		container.New(
+			layout.NewHBoxLayout(),
+			btnBack,
+			layout.NewSpacer(),
+		),
+	)
+
+	// Add a title
+	title := canvas.NewText("Add a new model", theme.Color(theme.ColorNameForeground))
+	title.TextSize = 32
+	title.Alignment = fyne.TextAlignCenter
+
+	// Add name field
+	nameTitle := canvas.NewText("Name", theme.Color(theme.ColorNameForeground))
+	nameOption := widget.NewEntry()
+	nameOption.Text = "My OVI"
+
+	// Add model select button
+	selectTitle := canvas.NewText("Model", theme.Color(theme.ColorNameForeground))
+	selectOption := widget.NewSelect(models, func(s string) {})
+
+	// Make the form
+	buildForm := func(form *fyne.Container) {
+		form.Objects = append(form.Objects,
+			nameTitle, nameOption,
+			selectTitle, selectOption,
+			widget.NewSeparator(), widget.NewSeparator(),
+		)
+	}
+	form := container.New(layout.NewFormLayout())
+	buildForm(form)
+
+	// Add a submit button
+	btn := bigBTN("Submit", 60, 15, func() {
+		saveSelector(form)
+	})
+
+	selectOption.OnChanged = func(s string) {
+		form.RemoveAll()
+		buildForm(form)
+		formSelector(s, form)
+	}
+
+	content := container.New(
+		layout.NewVBoxLayout(),
+		navbar,
+		title,
+		layout.NewSpacer(),
+		form,
+		layout.NewSpacer(),
+		btn,
+	)
+	addWindow.SetContent(content)
+	addWindow.Resize(fyne.NewSquareSize(512))
+	addWindow.Show()
+}
+
 func main() {
 	// Start fyne
 	App = app.New()
-	MainWindow = App.NewWindow("Machina-Maestro")
+	MainWindow = App.NewWindow("Machina Maestro")
 	MainWindow.SetOnClosed(func() {
 		App.Quit()
 	})
@@ -145,10 +351,10 @@ func main() {
 	// Make sure we don't crash
 	defer func() {
 		if r := recover(); r != nil {
-			fatalError(fmt.Errorf("Unhandled crash"))
+			fatalError(fmt.Errorf("unhandled crash"))
 		}
 
-		if err := os.WriteFile("log.txt", []byte(logger.String()), os.ModePerm); err != nil {
+		if err := os.WriteFile("log.txt", logger.Bytes(), os.ModePerm); err != nil {
 			log.Println(err)
 			fmt.Println(logger.String())
 			panic(err)
@@ -157,15 +363,70 @@ func main() {
 
 	// Render app
 	go func() {
-		initialize()
+		startup(1)
 
-		clock := widget.NewLabel("")
-		MainWindow.SetContent(clock)
-		for range time.Tick(time.Second) {
-			clock.SetText(time.Now().Format("Time: 03:04:05"))
-		}
+		landingPage(MainWindow)
 	}()
 
 	// Display app
 	MainWindow.ShowAndRun()
+}
+
+var models []string = []string{
+	"OVI MK2",
+	"OVI MK3",
+	"OVI Mini",
+}
+
+func formSelector(s string, form *fyne.Container) {
+	switch s {
+	case "OVI MK2":
+		form_OVI_MK2(form)
+	case "OVI MK3":
+		form_OVI_MK3(form)
+	case "OVI Mini":
+		form_OVI_Mini(form)
+	}
+}
+
+func saveSelector(form *fyne.Container) {
+	s := form.Objects[3].(*widget.Select).Selected
+	switch s {
+	case "OVI MK2":
+		save_OVI_MK2(form)
+	case "OVI MK3":
+		save_OVI_MK3(form)
+	case "OVI Mini":
+		save_OVI_Mini(form)
+	}
+}
+
+func form_OVI_MK2(form *fyne.Container) {
+	// Add IP entry
+	ipName := canvas.NewText("IP", theme.Color(theme.ColorNameForeground))
+	ipEntry := widget.NewEntry()
+	ipEntry.Text = "192.168.4.1"
+
+	form.Objects = append(form.Objects,
+		ipName, ipEntry,
+	)
+
+}
+
+func save_OVI_MK2(form *fyne.Container) {
+	fmt.Println("Saved a valid OVI MK2")
+}
+
+func form_OVI_MK3(form *fyne.Container) {
+}
+
+func save_OVI_MK3(form *fyne.Container) {
+	fmt.Println("Saved a valid OVI MK3")
+}
+
+func form_OVI_Mini(form *fyne.Container) {
+}
+
+func save_OVI_Mini(form *fyne.Container) {
+	fmt.Println("Saved a valid OVI Mini")
 }
