3a65291f47a0b84c0d4f5b46622eb370c2807977
Petarda-Atomica <perfect.minecraft.andrei@gmail.com>
2024-11-17 10:35:16 +0200

    Created an empty splash screen window

---
+++ windows/splash_screen/screen.go
@@ -0,0 +1,12 @@
+package splashscreen
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
+func Launch(App fyne.App) {
+	window := App.NewWindow("Launching...")
+
+	window.SetContent(widget.NewLabel("Hello, World!"))
+}
