c6a99a8c2a28dcb40851454b8bc19762288ecc33
Petarda-Atomica <perfect.minecraft.andrei@gmail.com>
2024-11-17 14:26:32 +0200

    Added a button to the home screen

--- windows/home_screen/screen.go
+++ windows/home_screen/screen.go
@@ -1,11 +1,14 @@
 package homescreen
 
 import (
+	"sync"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 )
 
 func Launch(window fyne.Window) {
@@ -19,6 +22,13 @@ func Launch(window fyne.Window) {
 	logo := getLogo()
 	logo.SetMinSize(fyne.NewSquareSize(400))
 
+	// Make a button
+	var waitPressGroup sync.WaitGroup
+	waitPressGroup.Add(1)
+	btn := widget.NewButton("Pick your OVI", func() {
+		waitPressGroup.Done()
+	})
+
 	// Make content
 	content := container.New(
 		layout.NewVBoxLayout(),
@@ -26,9 +36,14 @@ func Launch(window fyne.Window) {
 		layout.NewSpacer(),
 		logo,
 		layout.NewSpacer(),
+		btn,
+		layout.NewSpacer(),
 	)
 
 	// Show window
 	window.SetContent(content)
 	window.Show()
+
+	// Wait the button press before exiting
+	waitPressGroup.Wait()
 }
