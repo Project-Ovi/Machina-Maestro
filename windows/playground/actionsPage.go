22a0710e573d1a665894c7f9b2c8a9ce45386052
Petarda-Atomica <perfect.minecraft.andrei@gmail.com>
2024-11-25 20:05:23 +0200

    Made an Action add window

--- windows/playground/actionsPage.go
+++ windows/playground/actionsPage.go
@@ -2,6 +2,8 @@ package playground
 
 import (
 	"fmt"
+	"log"
+	"sync"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -16,9 +18,99 @@ import (
 var actionsCollection []helper.Action
 var actionsPlayQueue []int
 
+func actionCreateDialog() {
+	// Make sure the function doesn't exit prematurely
+	var dialogWait sync.WaitGroup
+	dialogWait.Add(1)
+
+	// Create a window
+	App := fyne.CurrentApp()
+	window := App.NewWindow("Create an Action")
+	window.SetOnClosed(dialogWait.Done)
+
+	// Create a title
+	titleObj := canvas.NewText(window.Title(), theme.Color(theme.ColorNameForeground))
+	titleObj.TextSize = theme.Size(theme.SizeNameHeadingText)
+	titleObj.Alignment = fyne.TextAlignCenter
+	titleObj.TextStyle.Bold = true
+
+	// Create name entry
+	nameTitleObj := widget.NewLabel("Name:")
+	nameEntryObj := widget.NewEntry()
+
+	// Create description entry
+	descriptionTitleObj := widget.NewLabel("Description:")
+	descriptionEntryObj := widget.NewEntry()
+
+	// Create create button
+	createBTN := widget.NewButtonWithIcon("Create", theme.Icon(theme.IconNameContentAdd), func() {})
+	createBTN.OnTapped = func() {
+		createBTN.Disable()
+
+		// Validate name
+		if nameEntryObj.Text == "" {
+			log.Println("Action name can't be empty")
+			return
+		} else if err := nameEntryObj.Validate(); err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Make sure the name is unique
+		for _, val := range actionsCollection {
+			if val.Name == nameEntryObj.Text {
+				log.Println("An action with this name already exists:", val.Name)
+				return
+			}
+		}
+
+		// Validate description
+		if err := descriptionEntryObj.Validate(); err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Create action
+		actionsCollection = append(actionsCollection, helper.Action{
+			Name:        nameEntryObj.Text,
+			Description: descriptionEntryObj.Text,
+		})
+
+		// Close window
+		window.Close()
+	}
+
+	// Make content
+	content := container.New(
+		layout.NewVBoxLayout(),
+		titleObj,
+		layout.NewSpacer(),
+		container.New(
+			layout.NewFormLayout(),
+			nameTitleObj, nameEntryObj,
+			descriptionTitleObj, descriptionEntryObj,
+		),
+		layout.NewSpacer(),
+		createBTN,
+		layout.NewSpacer(),
+	)
+
+	// Show window
+	window.SetContent(content)
+	window.Show()
+
+	// Await an exit signal
+	dialogWait.Wait()
+}
+
 func actionsPage(content *fyne.Container) {
 	// Make navbar
 	navbarAddBTN := widget.NewButtonWithIcon("", theme.Icon(theme.IconNameContentAdd), func() {})
+	navbarAddBTN.OnTapped = func() {
+		navbarAddBTN.Disable()
+		actionCreateDialog()
+		actionsPage(content)
+	}
 	actionsNavbar := container.New(
 		layout.NewStackLayout(),
 		canvas.NewRectangle(theme.Color(theme.ColorNameHeaderBackground)),
